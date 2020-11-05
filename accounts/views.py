a294416015a456e60fe7afcb573b5a7ef0dada15
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-05 10:15:54 +0600

    Added basic view of home, customer and products

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hi')
+    return HttpResponse('home')
+
+def products(request):
+    return HttpResponse('Products')
+
+def customer(request):
+    return HttpResponse('customer')
