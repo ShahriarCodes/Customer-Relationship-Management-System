4ce4d31b57245d268092a87792ab3d063b9e0dfc
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-06 19:17:26 +0600

    added tags in Products model (manyto many) ,Migrations

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
             ('Indoor', 'Indoor'),
@@ -23,13 +30,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=10000, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=200, null=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -46,7 +47,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    tags = models.ManyToManyField(Tag)
+    
 
     def __str__(self):
         return self.status
