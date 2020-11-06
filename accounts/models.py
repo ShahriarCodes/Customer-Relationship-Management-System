9a74642112c254f4b25b88355bed407d4c3df224
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-06 18:07:43 +0600

    migrations

--- accounts/models.py
+++ accounts/models.py
@@ -33,8 +33,8 @@ class Order(models.Model):
             ('Delivered', 'Delivered'),
             )
 
-    customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL) #Referencing Customer model 
-    product = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
+    customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL) #Referencing Customer model
+    product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
 
