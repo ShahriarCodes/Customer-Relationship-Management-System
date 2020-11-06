1bcee9c580323536d07e70c0c6c2dcc0392298dc
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-06 19:23:25 +0600

    Added blank=True in description

--- accounts/models.py
+++ accounts/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=10000, null=True)
+    description = models.CharField(max_length=10000, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
 
@@ -47,7 +47,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    
+
 
     def __str__(self):
         return self.status
