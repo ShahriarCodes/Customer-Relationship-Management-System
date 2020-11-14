e367a6bdc198bbf34432564365b333f669b41c33
shahriar100 <shahriar.hasan100@gmail.com>
2020-11-14 17:01:10 +0600

    Added password reset urls in urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -8,7 +10,7 @@ urlpatterns = [
 
     path('', views.home, name='dashboard'),
     path('user', views.userPage, name='user-page'),
-    
+
     path('account/', views.accountSettings, name="account"),
 
     path('products/', views.products, name='products'),
@@ -23,4 +25,11 @@ urlpatterns = [
     path('update_customer/<str:pk>', views.updateCustomer, name='update_customer'),
 
     path('create_product/', views.createProduct, name='create_product'),
+
+
+    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
